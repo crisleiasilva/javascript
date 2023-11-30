6bfd498ebd58ddbaad4f87e139377a1581ad204c
Crislei Aparecida da Silva <142687146+crisleiasilva@users.noreply.github.com>
2023-11-30 17:19:30 -0300

    Criei aula 011

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) { //condição simples, temos apenas 1 situação
+    console.log(`Você ultrapassou o limte de velocidade permitida.MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
