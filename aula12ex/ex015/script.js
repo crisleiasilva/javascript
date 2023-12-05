0b95a18096233e312d9f17868ff4f42dee7f866f
Crislei Aparecida da Silva <142687146+crisleiasilva@users.noreply.github.com>
2023-12-05 16:33:48 -0300

    Atualizei ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -7,7 +7,44 @@ function verificar() {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
-        var idade = ano -Number(fano.value)
+        var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src','bebe-menino1.jpg')
+            } else if ( idade < 25) {
+                //jovem
+                img.setAttribute('src', 'homem-jovem1.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homem-adulto1.jpg')
+            }else {
+                // idoso
+                img.setAttribute('src', 'homem-velho1.jpg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher' 
+            if(idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'bebe-menina1.jpg')
+            } else if ( idade < 24) {
+                //jovem
+                img.setAttribute('src','mulher-jovem1.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'mulher-adulta1.jpg')
+            }else {
+                // idoso
+                img.setAttribute('src','mulher-idosa1.jpg')
+            }
+
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
