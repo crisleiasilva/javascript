b159d7721344be1f10400226d0e6e05d35154f3a
Crislei Aparecida da Silva <142687146+crisleiasilva@users.noreply.github.com>
2023-12-07 14:21:03 -0300

    Aula03 finalizada

--- aula03/script.js
+++ aula03/script.js
@@ -1,14 +1,24 @@
+
 function calcular() {
-    var tnome = window.document.getElementById('txtnome')
+
+    var nome = window.document.getElementById('txtnome').value
     var tsald = window.document.getElementById('txsal')
     var dep = window.document.getElementById('txdep')
     var res = window.document.querySelector('div#res')
-    var s1 = Number (tsald.value)
-    var s2 = Number(dep.value)
+    var s1 = Number.parseFloat (tsald.value)
+    var s2 = Number.parseFloat(dep.value)
     var total = s1 + s2
-    res.innerHTML = `Olá, ${tnome} Seu Saldo Atual é de <strong> ${total}</strong>`
-    
-    
-   
+    res.innerHTML += ` Olá, ${nome} seu SALDO é de <strong> R$ ${total.toFixed(2)}</strong>`
+    if (total < 400) {
+        res.innerHTML += `<strong> SALDO BAIXO</strong> deposite valor maior`
+   } else if (total > 400 && total <= 10000) {
+       res.innerHTML += 'SALDO ALTO faça um investimento'
+   // } else  {
+      //  res.innerHTML = '<strong>CONTA BLOQUEADA</strong>'
+    }  
 
-}
+    
+
+        }
+
+
