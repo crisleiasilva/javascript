83584a1deaec4d0135f1ff072a712e69c9d025cf
Crislei Aparecida da Silva <142687146+crisleiasilva@users.noreply.github.com>
2023-12-07 08:25:21 -0300

    Atualizei aula03

--- aula03/script.js
+++ aula03/script.js
@@ -1,12 +1,12 @@
 function calcular() {
-    var tnome = window.document.querySelector('')
+    var tnome = window.document.getElementById('txtnome')
     var tsald = window.document.getElementById('txsal')
     var dep = window.document.getElementById('txdep')
-    var res = window.document.getElementById('res')
+    var res = window.document.querySelector('div#res')
     var s1 = Number (tsald.value)
     var s2 = Number(dep.value)
     var total = s1 + s2
-    res.innerHTML = `${txtnome} Seu Saldo Atual é de <strong> ${total}</strong>`
+    res.innerHTML = `Olá, ${tnome} Seu Saldo Atual é de <strong> ${total}</strong>`
     
     
    
