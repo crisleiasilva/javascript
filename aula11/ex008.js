ffde4cb2d1d93f15d9c8085ee687fbc62ed050c7
crisleiasilva <crisleisilva1905@gmail.com>
2023-12-11 11:54:44 -0300

    Atualizei aula11

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,4 +1,4 @@
-var vel = 80
+var vel = 85
 console.log(`A velocidade do seu carro é ${vel} km/h`)
 if(vel > 60) { //condição simples, temos apenas 1 situação
     console.log(`Você ultrapassou o limite de velocidade permitida.MULTADO!`)
